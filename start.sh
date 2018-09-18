8af3c85f9e00ecbb5d66aec7174f39b5319c48e3
alejo4373 <alejo4373@gmail.com>
2018-09-18 11:57:26 -0400

    bash script to start server if not already running

--- start.sh
+++ start.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-## Start server on new terminal and release
-xterm -hold -e "cd $GOPATH/src/github.com/HeadlightLabs/Tournament-API/ && ./run.sh " &  
+## Check if pgrep is available
+if ! [ -x "$(command -v pgrep)" ]
+then 
+  echo "command pgrep is not available in this system";
+  exit 1;
+fi
+
+## Find proccess id of server if already running in xterm
+psid=`pgrep -fi "xterm.*run.sh"`
+
+## If psid not found start server, else exit
+if [ -z ${psid} ] 
+then
+  echo "Starting server";
+
+  ## Start server on new terminal and release
+  xterm -hold -e "cd $GOPATH/src/github.com/HeadlightLabs/Tournament-API/ && ./run.sh " &  
+else
+  echo "Server already running pid: ${psid}";
+  exit 1;
+fi
