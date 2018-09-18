1bfc27369242179abb4e1cec8d3fff455f67d36c
alejo4373 <alejo4373@gmail.com>
2018-09-18 14:44:10 -0400

    verbose test

--- __tests__/sendRequest.test.js
+++ __tests__/sendRequest.test.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { sendRequest } = require('../sendRequest');
 
 describe('sendRequest', () => {
-  it('sends a request', () => {
+  it('sends /register request and receive id', () => {
     expect.assertions(1)
     return sendRequest('/register', {}).then(res => {
       expect(res.status).toHaveProperty('id');
